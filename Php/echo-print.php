2ad78bea0fe62c4cd45e66f17cf56efba9c5def8
arielmell0 <arielmello@rede.ulbra.br>
2021-01-24 11:47:13 -0300

    cursos front end html - css - js

---
+++ Php/echo-print.php
@@ -0,0 +1,5 @@
+Os comandos echo e print são utilizados para impressão de conteúdos em tela
+<?php
+echo "Teste de impressão <br>";
+print "Teste de impressão com print";
+?>
---
+++ Php/variaveis.php
@@ -0,0 +1,18 @@
+<?php
+
+//A váriavel deve iniciar com $ e não deve ter caracteres especiais (exceto "_")
+//Não pode iniciar com espaços ou números
+
+$int = 2;
+
+$txt = 'texto';
+
+$fracionado = 10.89;
+
+echo $int;
+
+echo $txt;
+
+echo $fracionado;
+
+?>
